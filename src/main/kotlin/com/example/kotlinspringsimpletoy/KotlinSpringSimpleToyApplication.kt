04710e634e5ae293d55d4940e5abe3bb151425b7
sunwng <sunwgna@gmail.com>
2022-11-09 23:51:49 +0900

    init

---
+++ src/main/kotlin/com/example/kotlinspringsimpletoy/KotlinSpringSimpleToyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringsimpletoy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringSimpleToyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringSimpleToyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringsimpletoy/KotlinSpringSimpleToyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringsimpletoy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringSimpleToyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
